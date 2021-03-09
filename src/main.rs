8fac0021e5b1ce87c2f78bf7b50b361dcec9e451
Eric Wätke <ewaetke10@gmail.com>
2021-03-09 21:36:59 +0100

    GraphQL Server with some Nintendo Data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate juniper;
+#[macro_use]
+extern crate diesel;
+
+use std::io;
+use std::sync::Arc;
+
+use actix_web::{web, App, Error, HttpResponse, HttpServer, Responder};
+use futures::future::Future;
+use juniper::http::graphiql::graphiql_source;
+use juniper::http::GraphQLRequest;
+
+mod graphql_schema;
+mod schema;
+
+use crate::graphql_schema::{create_schema, Schema};
+
+fn main() -> io::Result<()> {
+    let schema = Arc::new(create_schema());
+    HttpServer::new(move || {
+        App::new()
+            .data(schema.clone())
+            .service(web::resource("/graphql").route(web::post().to_async(graphql)))
+            .service(web::resource("/graphiql").route(web::get().to(graphiql)))
+    })
+    .bind("localhost:8080")?
+    .run()
+}
+
+fn graphql(
+    st: web::Data<Arc<Schema>>,
+    data: web::Json<GraphQLRequest>
+) -> impl Future<Item = HttpResponse, Error = Error> {
+    web::block(move || {
+        let res = data.execute(&st, &());
+        Ok::<_, serde_json::error::Error>(serde_json::to_string(&res)?)
+    })
+    .map_err(Error::from)
+    .and_then(|user| {
+        Ok(HttpResponse::Ok()
+            .content_type("application/json")
+            .body(user))
+    })
+}
+
+fn graphiql() -> HttpResponse {
+    let html = graphiql_source("http://localhost:8080/graphql");
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(html)
 }
