f50ce61007e7ef6b8a662b59efacab6c5974801a
Eric Wätke <ewaetke10@gmail.com>
2021-03-21 18:16:26 +0100

    Diesel POSTGRES Sync

--- src/graphql_schema.rs
+++ src/graphql_schema.rs
@@ -8,66 +8,66 @@ use dotenv::dotenv;
 
 use juniper::{EmptyMutation, RootNode};
 
-use crate::schema::members;
+// use crate::schema::members;
 
-#[derive(Queryable)]
-struct Member {
-	pub id: i32,
-	pub name: String,
-	pub knockouts: i32,
-	pub team_id: i32
-}
+// #[derive(Queryable)]
+// struct Member {
+// 	pub id: i32,
+// 	pub name: String,
+// 	pub knockouts: i32,
+// 	pub team_id: i32
+// }
 
-#[juniper::object(description = "A member of a team")]
-impl Member {
-	pub fn id(&self) -> i32 {
-		self.id  
-	}
+// #[juniper::object(description = "A member of a team")]
+// impl Member {
+// 	pub fn id(&self) -> i32 {
+// 		self.id  
+// 	}
 
-	pub fn name(&self) -> &str {
-		self.name.as_str()
-	}
+// 	pub fn name(&self) -> &str {
+// 		self.name.as_str()
+// 	}
 
-	pub fn knockouts(&self) -> i32{
-		self.knockouts
-	}
+// 	pub fn knockouts(&self) -> i32{
+// 		self.knockouts
+// 	}
 
-	pub fn team_id(&self) -> i32 {
-		self.team_id
-	}
-}
+// 	pub fn team_id(&self) -> i32 {
+// 		self.team_id
+// 	}
+// }
 
 
 // User Table
 
 // use crate::schema::users;
 
-#[derive(Queryable)]
-struct User {
-	pub id: i32,
-	pub name: String,
-	pub knockouts: i32,
-	pub team_id: i32
-}
+// #[derive(Queryable)]
+// struct User {
+// 	pub id: i32,
+// 	pub name: String,
+// 	pub knockouts: i32,
+// 	pub team_id: i32
+// }
 
-#[juniper::object(description = "A single User")]
-impl User {
-	pub fn id(&self) -> i32 {
-		self.id  
-	}
+// #[juniper::object(description = "A single User")]
+// impl User {
+// 	pub fn id(&self) -> i32 {
+// 		self.id  
+// 	}
 
-	pub fn name(&self) -> &str {
-		self.name.as_str()
-	}
+// 	pub fn name(&self) -> &str {
+// 		self.name.as_str()
+// 	}
 
-	pub fn knockouts(&self) -> i32{
-		self.knockouts
-	}
+// 	pub fn knockouts(&self) -> i32{
+// 		self.knockouts
+// 	}
 
-	pub fn team_id(&self) -> i32 {
-		self.team_id
-	}
-}
+// 	pub fn team_id(&self) -> i32 {
+// 		self.team_id
+// 	}
+// }
 
 // use crate::schema::teams;
 
@@ -114,14 +114,15 @@ fn printCategory(category: Category) -> String {
 	}
 }
 
+#[derive(Queryable)]
 pub struct Company {
 	pub id: i32,
 	pub name: String,
 	pub category: Category,
-	pub tel: String,
-	pub mail: String,
-	pub web: String,
-	pub url: String,
+	pub tel: Nullable<String>,
+	pub mail: Nullable<String>,
+	pub web: Nullable<String>,
+	pub url: Nullable<String>,
 	pub approved: bool
 }
 
@@ -159,7 +160,17 @@ impl Company {
 		&self.approved
 	}
 
-	pub fn subscribers() -> Vec<User> {
+	// pub fn subscribers() -> Vec<User> {
+	// 	// use crate::schema::members::dsl::*;
+	// 	// let connection = establish_connection();
+	// 	// members
+	// 	// 	.filter(team_id.eq(self.id))
+	// 	// 	.limit(100)
+	// 	// 	.load::<Member>(&connection)
+	// 	// 	.expect("Error loading members")
+	// 	vec![]
+	// }
+	pub fn subscribers() -> &bool {
 		// use crate::schema::members::dsl::*;
 		// let connection = establish_connection();
 		// members
@@ -167,7 +178,7 @@ impl Company {
 		// 	.limit(100)
 		// 	.load::<Member>(&connection)
 		// 	.expect("Error loading members")
-		vec![]
+		&true
 	}
 
 	pub fn subscriber_count() -> i32 {
@@ -181,55 +192,47 @@ pub struct QueryRoot;
 fn establish_connection() -> PgConnection {
 	dotenv().ok();
 	// let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-	let database_url = "postgres://postgres:root@localhost/graph-ql";
+	let database_url = "postgres://postgres:root@localhost/luebz-liefert-database";
 	PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
 }
 
 #[juniper::object]
 impl QueryRoot {
-	fn members() -> Vec<Member> {
-		use crate::schema::members::dsl::*;
+	fn companies() -> Vec<Company> {
+		use crate::schema::companies::dsl::*;
 		let connection = establish_connection();
-		members
+		companies
 			.limit(100)
-			.load::<Member>(&connection)
-			.expect("Error loading members")
-	}
-	fn teams() -> Vec<Team> {
-		use crate::schema::teams::dsl::*;
-		let connection = establish_connection();
-		teams
-			.limit(100)
-			.load::<Team>(&connection)
-			.expect("Error loading teams")
+			.load::<Company>(&connection)
+			.expect("Error could not load companies")
 	}
 }
 
 
 pub struct MutationRoot;
 
-#[juniper::object]
-impl MutationRoot {
-	fn create_member(data: NewMember) -> Member {
-		let connection = establish_connection();
-		diesel::insert_into(members::table)
-		.values(&data)
-		.get_result(&connection)
-		.expect("Error saving new post")
-	}
-}
+// #[juniper::object]
+// impl MutationRoot {
+// 	fn create_member(data: NewMember) -> Member {
+// 		let connection = establish_connection();
+// 		diesel::insert_into(members::table)
+// 		.values(&data)
+// 		.get_result(&connection)
+// 		.expect("Error saving new post")
+// 	}
+// }
 
-#[derive(juniper::GraphQLInputObject, Insertable)]
-#[table_name = "members"]
-	pub struct NewMember {
-	pub name: String,
-	pub knockouts: i32,
-	pub team_id: i32,
-}
+// #[derive(juniper::GraphQLInputObject, Insertable)]
+// #[table_name = "members"]
+// 	pub struct NewMember {
+// 	pub name: String,
+// 	pub knockouts: i32,
+// 	pub team_id: i32,
+// }
 
 
-pub type Schema = RootNode<'static, QueryRoot, MutationRoot>;
+pub type Schema = RootNode<'static, QueryRoot, EmptyMutation<()>>;
 
 pub fn create_schema() -> Schema {
-	Schema::new(QueryRoot {}, MutationRoot {})
+  Schema::new(QueryRoot {}, EmptyMutation::new())
 }
--- src/schema.rs
+++ src/schema.rs
@@ -1,22 +1,86 @@
 table! {
-    members (id) {
-        id -> Int4,
-        name -> Varchar,
-        knockouts -> Int4,
-        team_id -> Int4,
+    categories (id) {
+        id -> Int8,
+        category_name -> Nullable<Varchar>,
     }
 }
 
 table! {
-    teams (id) {
-        id -> Int4,
-        name -> Varchar,
+    companies (id) {
+        id -> Int8,
+        name -> Nullable<Varchar>,
+        category -> Nullable<Varchar>,
+        phone -> Nullable<Varchar>,
+        mail -> Nullable<Varchar>,
+        web -> Nullable<Varchar>,
+        description -> Nullable<Text>,
+        whatsapp -> Nullable<Int8>,
+        approved -> Bool,
     }
 }
 
-joinable!(members -> teams (team_id));
+table! {
+    login_tokens (id) {
+        id -> Int8,
+        account_id -> Int8,
+        token -> Varchar,
+        date -> Date,
+    }
+}
+
+table! {
+    subscribed_categories (id) {
+        id -> Int8,
+        account_id -> Nullable<Int8>,
+        category_id -> Nullable<Int8>,
+    }
+}
+
+table! {
+    subscribed_companies (id) {
+        id -> Int8,
+        account_id -> Nullable<Int8>,
+        company_id -> Nullable<Int8>,
+        date -> Nullable<Date>,
+    }
+}
+
+table! {
+    subscriptions (id) {
+        id -> Int8,
+        user_id -> Nullable<Int8>,
+        #[sql_name = "type"]
+        type_ -> Nullable<Varchar>,
+        date -> Nullable<Date>,
+        adress -> Nullable<Varchar>,
+        auth -> Nullable<Varchar>,
+        p256dh -> Nullable<Varchar>,
+    }
+}
+
+table! {
+    users (id) {
+        id -> Int8,
+        email -> Nullable<Varchar>,
+        password -> Nullable<Varchar>,
+        name -> Nullable<Varchar>,
+        account_type -> Nullable<Varchar>,
+        unique_identifier -> Nullable<Varchar>,
+        register_date -> Nullable<Date>,
+        varified -> Nullable<Bool>,
+        last_action_date -> Nullable<Date>,
+        company -> Nullable<Int8>,
+    }
+}
+
+joinable!(subscribed_categories -> categories (category_id));
 
 allow_tables_to_appear_in_same_query!(
-    members,
-    teams,
+    categories,
+    companies,
+    login_tokens,
+    subscribed_categories,
+    subscribed_companies,
+    subscriptions,
+    users,
 );
